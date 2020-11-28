0e1b40855dc13bc59bb4e208c309f195133b8516
bangadam <bangadam.dev@gmail.com>
2020-11-28 17:33:41 +0700

    update

---
+++ module/constants.py
@@ -0,0 +1,4 @@
+class Constants:
+    HOST_API = 'https://scholar.google.com'
+    SEARCH_PROFILE_API = HOST_API + '/citations?hl=id&view_op=search_authors&mauthors='
+    DETAIL_USER_API = HOST_API + '/citations?hl=id&user='
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from smart_publication import app
+
+if __name__ == '__main__':
+    app.run()
